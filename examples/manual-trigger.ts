6c36142b965426168aaf0e1c65dea0d23a83d825
gabriel ruttner <gabe@hatchet.run>
2024-03-26 09:23:16 -0700

    fix: sync listener

--- examples/manual-trigger.ts
+++ examples/manual-trigger.ts
@@ -1,15 +1,13 @@
 import Hatchet from '../src/sdk';
-import { HatchetClient } from '../src/clients/hatchet-client/hatchet-client';
-import { StepRunEvent } from '../src/clients/listener/listener-client';
 
-const hatchet: HatchetClient = Hatchet.init();
+const hatchet = Hatchet.init();
 
 async function main() {
   const workflowRunId = await hatchet.admin.run_workflow('example', {});
 
-  hatchet.listener.on(workflowRunId, async (event: StepRunEvent) => {
-    console.log('Received event', event);
-  });
+  for await (const event of hatchet.listener.stream(workflowRunId)) {
+    console.log('event received', event);
+  }
 }
 
 main();

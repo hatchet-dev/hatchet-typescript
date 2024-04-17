ffadaad5927751b2a70da6ce8bb3f4a68a35f9e2
gabriel ruttner <gabriel.ruttner@gmail.com>
2024-04-17 14:35:58 -0600

    chore: example

--- src/clients/listener/listener-client.ts
+++ src/clients/listener/listener-client.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line max-classes-per-file
-import { Channel, ClientFactory } from 'nice-grpc';
+import { Channel, ClientFactory, Status } from 'nice-grpc';
 import { EventEmitter } from 'events';
 import {
   DispatcherClient as PbDispatcherClient,
@@ -10,11 +10,11 @@ import {
 } from '@hatchet/protoc/dispatcher';
 import { ClientConfig } from '@clients/hatchet-client/client-config';
 import { Logger } from '@hatchet/util/logger';
+import sleep from '@hatchet/util/sleep';
 import { Api } from '../rest';
 
 const DEFAULT_EVENT_LISTENER_RETRY_INTERVAL = 5; // seconds
 const DEFAULT_EVENT_LISTENER_RETRY_COUNT = 5;
-const DEFAULT_EVENT_LISTENER_POLL_INTERVAL = 5000; // milliseconds
 
 // eslint-disable-next-line no-shadow
 export enum RunEventType {
@@ -68,21 +68,39 @@ export class GrpcPooledListener {
     this.init();
   }
 
-  private async init() {
-    this.listener = this.client.client.subscribeToWorkflowRuns(this.request());
-
-    for await (const event of this.listener) {
-      const emitter = this.subscribers[event.workflowRunId];
-      if (emitter) {
-        emitter.responseEmitter.emit('response', event);
-        if (event.eventType === WorkflowRunEventType.WORKFLOW_RUN_EVENT_TYPE_FINISHED) {
-          delete this.subscribers[event.workflowRunId];
-        }
-      }
+  private async init(retries = 0) {
+    if (retries > DEFAULT_EVENT_LISTENER_RETRY_COUNT) return;
+    if (retries > 0) {
+      console.log('retrying');
+      await sleep(DEFAULT_EVENT_LISTENER_RETRY_INTERVAL * 1000);
     }
 
+    try {
+      this.client.logger.debug('Initializing listener');
+      this.listener = this.client.client.subscribeToWorkflowRuns(this.request());
+
+      for await (const event of this.listener) {
+        const emitter = this.subscribers[event.workflowRunId];
+        if (emitter) {
+          emitter.responseEmitter.emit('response', event);
+          if (event.eventType === WorkflowRunEventType.WORKFLOW_RUN_EVENT_TYPE_FINISHED) {
+            delete this.subscribers[event.workflowRunId];
+          }
+        }
+      }
+    } catch (e: any) {
+      if (e.code === Status.CANCELLED) {
+        return;
+      }
+      if (e.code === Status.UNAVAILABLE) {
+        await this.init(retries + 1);
+        return;
+      }
+
+      await this.init(retries + 1);
+    }
+    // TODO cleanup reconnect
     console.log('reconnecting');
-    // TODO reconnect
   }
 
   subscribe(request: SubscribeToWorkflowRunsRequest) {
@@ -113,7 +131,7 @@ export class ListenerClient {
 
   constructor(config: ClientConfig, channel: Channel, factory: ClientFactory, api: Api) {
     this.config = config;
-    this.client = factory.create(DispatcherDefinition, channel);
+    this.client = factory.create(DispatcherDefinition, channel, {});
     this.logger = new Logger(`Listener`, config.log_level);
     this.api = api;
     this.cache = new GrpcPooledListener(this);

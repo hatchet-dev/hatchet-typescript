598513d851bb97c14cc92057efc83041208771a2
Alexander Belanger <alexander@hatchet.run>
2024-06-17 09:26:48 -0400

    fix: linting

--- src/clients/admin/admin-client.ts
+++ src/clients/admin/admin-client.ts
@@ -108,15 +108,17 @@ export class AdminClient {
       additionalMetadata?: Record<string, string> | undefined;
     }
   ) {
+    let computedName = workflowName;
+
     try {
       if (this.config.namespace && !workflowName.startsWith(this.config.namespace)) {
-        workflowName = this.config.namespace + workflowName;
+        computedName = this.config.namespace + workflowName;
       }
 
       const inputStr = JSON.stringify(input);
 
       const resp = await this.client.triggerWorkflow({
-        name: workflowName,
+        name: computedName,
         input: inputStr,
         ...options,
         additionalMetadata: options?.additionalMetadata

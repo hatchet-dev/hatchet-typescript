c2e945acd645db49d7a8fe8e4972925ce81e8d86
gabriel ruttner <gabriel.ruttner@gmail.com>
2024-10-29 11:29:54 -0400

    feat: generate cloud

--- src/clients/rest/api.ts
+++ src/clients/rest/api.ts
@@ -1,9 +1,21 @@
 import qs from 'qs';
 import { AxiosRequestConfig } from 'axios';
 import { Api } from './generated/Api';
+import { Api as CloudApi } from './generated/cloud/Api';
 
 const api = (serverUrl: string, token: string, axiosOpts?: AxiosRequestConfig) => {
   return new Api({
+    baseURL: serverUrl,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    paramsSerializer: (params) => qs.stringify(params, { arrayFormat: 'repeat' }),
+    ...axiosOpts,
+  });
+};
+
+export const cloudApi = (serverUrl: string, token: string, axiosOpts?: AxiosRequestConfig) => {
+  return new CloudApi({
     baseURL: serverUrl,
     headers: {
       Authorization: `Bearer ${token}`,

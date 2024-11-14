b62e3fe993de51976821eaecfad7cc77399f5775
gabriel ruttner <gabriel.ruttner@gmail.com>
2024-11-14 12:06:04 -0500

    feat: with details

--- src/examples/on-failure.ts
+++ src/examples/on-failure.ts
@@ -27,10 +27,16 @@ const workflow: Workflow = {
   onFailure: {
     name: 'on-failure-step',
     run: async (ctx) => {
+      // 👀 we can do things like perform cleanup logic
+      // or notify a user here
       return { onFailure: 'step' };
     },
   },
 };
+// ‼️
+
+// ❓ OnFailure With Details
+// Coming soon to TypeScript! https://github.com/hatchet-dev/hatchet-typescript/issues/447
 // ‼️
 
 async function main() {

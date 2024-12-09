e48ccf3a46058ba4bb5636b8df396fc5445cb2a1
abelanger5 <belanger@sas.upenn.edu>
2024-12-09 15:40:21 -0500

    fix: use `retrier` in event push (#489)
    * fix: make push async and use retrier
    
    * bump version
    
    * fix: events var
    
    * just use retrier
    
    * fix: unit tests
    
    * fix: unit tests again
    
    * lower retry interval + jitter

--- src/clients/event/event-client.ts
+++ src/clients/event/event-client.ts
@@ -30,6 +30,7 @@ export interface EventWithMetadata<T> {
 export class EventClient {
   config: ClientConfig;
   client: EventsServiceClient;
+  retrier: typeof retrier;
 
   logger: Logger;
 
@@ -37,6 +38,7 @@ export class EventClient {
     this.config = config;
     this.client = factory.create(EventsServiceDefinition, channel);
     this.logger = new Logger(`Dispatcher`, config.log_level);
+    this.retrier = retrier;
   }
 
   push<T>(type: string, input: T, options: PushEventOptions = {}) {
@@ -52,7 +54,7 @@ export class EventClient {
     };
 
     try {
-      const e = this.client.push(req);
+      const e = this.retrier(async () => this.client.push(req), this.logger);
       this.logger.info(`Event pushed: ${namespacedType}`);
       return e;
     } catch (e: any) {
@@ -85,9 +87,9 @@ export class EventClient {
     };
 
     try {
-      const e = this.client.bulkPush(req);
+      const res = this.retrier(async () => this.client.bulkPush(req), this.logger);
       this.logger.info(`Bulk events pushed for type: ${namespacedType}`);
-      return e;
+      return res;
     } catch (e: any) {
       throw new HatchetError(e.message);
     }

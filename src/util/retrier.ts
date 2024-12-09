f1aabfd2c28998bdb3b4cca18f6b1cd54d89149c
Alexander Belanger <alexander@hatchet.run>
2024-12-09 15:35:20 -0500

    lower retry interval + jitter

--- src/util/retrier.ts
+++ src/util/retrier.ts
@@ -1,9 +1,9 @@
 import { Logger } from './logger';
 import sleep from './sleep';
 
-const DEFAULT_RETRY_INTERVAL = 1; // seconds
+const DEFAULT_RETRY_INTERVAL = 0.1; // seconds
 const DEFAULT_RETRY_COUNT = 8;
-const MAX_JITTER = 400; // milliseconds
+const MAX_JITTER = 100; // milliseconds
 
 export async function retrier<T>(
   fn: () => Promise<T>,
